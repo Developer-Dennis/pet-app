ab50b261830de23361d8f70c7c77e225edda19fc
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-21 09:14:06 +0300

    the single vue for app pet page

---
+++ src/components/Pet.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="pet">
+        <!-- <h3>{{ pet.name }}</h3> -->
+        <Card >
+            <template #header>
+                <img src="https://www.primefaces.org/wp-content/uploads/2020/02/primefacesorg-primevue-2020.png" />
+            </template>
+            <template #title>
+                {{ pet.name }}
+            </template>
+            <template #subtitle>
+               Age: {{pet.age}}
+            </template>
+            <!-- <template #content>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt
+                quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!</p>
+            </template> -->
+            <template #footer>
+                <Button icon="pi pi-times" label="Remove" class="p-button-danger" style="margin-right: 4em" />
+                <Button icon="pi pi-heart" class="p-button-rounded p-button-danger" />
+                <Button icon="pi pi-heart" class="p-button-rounded p-button-danger p-button-outlined" />
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        pet: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
