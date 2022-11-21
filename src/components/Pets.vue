1d99a83c632bca67a27fc919f13abafe49408cf1
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-21 09:14:54 +0300

    the file for vue app landing page

---
+++ src/components/Pets.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="grid">
+        <div  class="col-12 md:col-6 lg:col-3" v-for="pet in pets" :key="pet.id">
+            <!-- <h3>{{ pet.name }}</h3> -->
+            <Pet :pet="pet" />
+        </div>
+    </div>
+</template>
+
+<script>
+import Pet from '../components/Pet.vue'
+export default {
+    props:{
+        pets: Array
+    },
+    components:{
+        Pet
+    }
+}
+</script>
+
+<style>
+
+</style>
