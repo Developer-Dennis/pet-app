33c80f9ebb53d85ce455ddd0eb4bdd5f677c3692
Dennis Kemboi <45884254+Developer-Dennis@users.noreply.github.com>
2022-11-22 08:25:55 +0300

    imported all program dependencies

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import InputText from 'primevue/inputtext'
 
 import 'primevue/resources/themes/saga-blue/theme.css'    //theme
 import'primevue/resources/primevue.min.css'                //core css
@@ -18,4 +19,6 @@ app.use(router)
 app.use(PrimeVue, {ripple: true})
 app.component("Button", Button)
 app.component("Card",Card)
+app.component("InputText", InputText)
+
 app.mount('#app')
